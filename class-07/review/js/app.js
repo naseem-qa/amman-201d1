f9a64a5c438c98c6c6120b414f7a65102b6aa901
JB <jb.tellez@gmail.com>
2019-10-14 12:12:26 +0300

    adds in class changes for review of lab 6

--- class-07/review/js/app.js
+++ class-07/review/js/app.js
@@ -1,36 +1,39 @@
+'use strict'; // thanks Tazneem
+
 // You can define hours array like this...
-var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+// var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 // or this...
 
 
-// /* 
-//   Covert 24 hour clock hour to 12 hour clock 
-//   Just handles the whole hour, make it fancier to handle minutes too if you like
-// */
-// function twentyFourToTwelve(hour24) {
+/* 
+  Covert 24 hour clock hour to 12 hour clock 
+  Just handles the whole hour, make it fancier to handle minutes too if you like
+*/
+function twentyFourToTwelve(hour24) {
 
-//   if (hour24 == 24) {
-//     return '12am';
-//   } else if (hour24 == 12) {
-//     return '12pm';
-//   } else if (hour24 < 12) {
-//     return hour24 + 'am';
-//   } else {
-//     return (hour24 - 12) + 'pm';
-//   }
-// }
+  if (hour24 == 24) {
+    return '12am';
+  } else if (hour24 == 12) {
+    return '12pm';
+  } else if (hour24 < 12) {
+    return hour24 + 'am';
+  } else {
+    return (hour24 - 12) + 'pm';
+  }
+}
 
-// var hours = [];
+var hours = [];
 
-// for (i = 6; i <= 19; i++) {
-//   hours.push(twentyFourToTwelve(i));
-// }
+for (var i = 6; i <= 19; i++) {
+  hours.push(twentyFourToTwelve(i));
+}
 
 
 
 // create shop objects
 var seattle = {
+  location: 'Seattle',
   min: 23,
   max: 65,
   avgSale: 6.3,
@@ -39,7 +42,7 @@ var seattle = {
 
   getRandomCustomerCount: function () {
 
-    var range = this.max = this.min;
+    var range = this.max - this.min;
 
     var randomCount = Math.random() * range + this.min;
 
@@ -58,36 +61,102 @@ var seattle = {
     }
   },
 
-  render: function () {
+  render: function() {
+
     var container = document.getElementById('content-area');
+
     var h2 = document.createElement('h2');
-    h2.textContent = 'Seattle', //NOTE: this only part that is location specific. Shall we add a location property to object???
-      container.appendChild(h2);
-    var list = document.createElement('ul');
-    var totalCookies = 0;
-    container.appendChild(list);
-    for (var i = 0; i < this.hourlySales.length; i++) {
-      var listItem = document.createElement('li');
-      listItem.textContent = hours[i] + ': ' + this.hourlySales[i] + ' cookies';
-      list.appendChild(listItem);
+    container.appendChild(h2);
+    h2.textContent = this.location;
+
+    var ul = document.createElement('ul');
+    container.appendChild(ul);
+
+
+    for(var i = 0; i < hours.length; i++) {
+
+      var li = document.createElement('li');
+      ul.appendChild(li);
+
+      var salesInfo = hours[i] + ': ' + this.hourlySales[i] + ' cookies';
+
+      li.textContent = salesInfo;
+
     }
 
-    var listItem = document.createElement('li');
-    listItem.textContent = 'Total: ' + this.dailySales + ' cookies';
-    list.appendChild(listItem);
-  }
+    var li = document.createElement('li');
+    li.textContent = 'Total: ' + this.dailySales + ' cookies';
+    ul.appendChild(li);
+
+  },
 }
 
-seattle.generateHourlySales();
-seattle.render();
+var dubai = {
+  location: 'Dubai',
+  min: 11,
+  max: 38,
+  avgSale: 3.7,
+  hourlySales: [],
+  dailySales: 0,
 
-// var shops = [seattle]; // add more shops when ready
+  getRandomCustomerCount: function () {
 
-// for (var i = 0; i < shops.length; i++) {
-//   var shop = shops[i];
-//   shop.generateDailySales();
-//   shop.render();
-// }
+    var range = this.max - this.min;
+
+    var randomCount = Math.random() * range + this.min;
+
+    return Math.ceil(randomCount);
+  },
+
+  generateHourlySales: function () {
+
+    for (var i = 0; i < hours.length; i++) {
+
+      var cookiesSoldThisHour = Math.ceil(this.getRandomCustomerCount() * this.avgSale);
+
+      this.hourlySales.push(cookiesSoldThisHour);
+
+      this.dailySales += cookiesSoldThisHour;
+    }
+  },
+
+  render: function() {
+
+    var container = document.getElementById('content-area');
+
+    var h2 = document.createElement('h2');
+    container.appendChild(h2);
+    h2.textContent = this.location;
+
+    var ul = document.createElement('ul');
+    container.appendChild(ul);
+
+
+    for(var i = 0; i < hours.length; i++) {
+
+      var li = document.createElement('li');
+      ul.appendChild(li);
+
+      var salesInfo = hours[i] + ': ' + this.hourlySales[i] + ' cookies';
+
+      li.textContent = salesInfo;
+
+    }
+
+    var li = document.createElement('li');
+    li.textContent = 'Total: ' + this.dailySales + ' cookies';
+    ul.appendChild(li);
+
+  },
+}
+
+var shops = [seattle, dubai]; // add more shops when ready
+
+for (var i = 0; i < shops.length; i++) {
+  var shop = shops[i];
+  shop.generateHourlySales();
+  shop.render();
+}
 
 
 
